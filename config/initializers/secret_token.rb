082b4f5c85a8071f022a3589bcc0d65cc3220b39
Michael makarov <info1117@gmail.com>
2012-03-04 14:40:02 -0800

    Rails basic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sarger::Application.config.secret_token = '69102f7d162a19c364f77cf6c4f933af817b3e7d8780236323e67f83f27c880104786b8d150b3b1c120e5f370b9281203b7d0aac34b813e618479c8376fff464'
