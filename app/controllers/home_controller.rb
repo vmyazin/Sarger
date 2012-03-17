9bd40121de29bf1bbda9a49c8768870d6cb723fb
Vasily Myazin <vm@konspiredesign.com>
2012-03-17 15:55:59 -0700

    Styled the app some more

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  
   def index
+    @tasks = Task.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tasks }
+    end
   end
 
 end
